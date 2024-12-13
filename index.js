f89013ad7ba4c2b30eb3871a128fa29cbfae9f17
123-ne <linannetw@gmail.com>
2024-12-13 20:03:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const crash = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/crash.mp3');
-const kickbass = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/kick-bass.mp3');
-const snare = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/snare.mp3');
-const tom1 = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/tom-1.mp3');
-const tom2 = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/tom-2.mp3');
-const tom3 = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/tom-3.mp3');
-const tom4 = new Audio('https://github.com/123-ne/113306010_Lab4/blob/main/tom-4.mp3');
+const crash = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/crash.mp3');
+const kickbass = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/kick-bass.mp3');
+const snare = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/snare.mp3');
+const tom1 = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/tom-1.mp3');
+const tom2 = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/tom-2.mp3');
+const tom3 = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/tom-3.mp3');
+const tom4 = new Audio('https://github.com/123-ne/113306010_Lab4/raw/main/tom-4.mp3');
 
 // Keyboard event listener
 document.addEventListener("keypress", function (e) {
